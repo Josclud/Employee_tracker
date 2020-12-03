0145a91d4a670f35d13d9b3f2c2158260fefe6b0
Joseph Wong-Ludwiczak <josclud@gmail.com>
2020-12-03 15:48:15 -0800

    andded inquirer and employee data

--- employees.js
+++ employees.js
@@ -1,28 +1,58 @@
 const mysql = require('mysql2')
+const inquirer = require('inquirer')
+require('console.table')
 
 const db = mysql.createConnection('mysql://root:Monkey23@localhost/employees_db')
 
-db.query('SELECT * FROM departments', (err, departments) => {
-  if (err) { console.log(err) }
-  console.log(departments)
-})
+let prompts = [
+  {
+    type: 'list',
+    name: 'name',
+    message: 'What would you like to view?',
+    choices: [
+      {
+        name: 'View all employees',
+        value: 'view employees'
+      },
+      // {
+      //   name: 'View all Departments',
+      //   value: 'view departments'
+      // }
+    ]
 
-db.query(`
-  SELECT roles.id, roles.title, roles.salary, departments.name AS department
-  FROM roles
-  LEFT JOIN departments
-  ON roles.departmentId = departments.id
-  `, (err, roles) => {
+  }
+]
+function handleMainPrompts(answers) {
+  console.log(answers.name);
+  db.query('SELECT * FROM employees', (err, employees) => {
     if (err) { console.log(err) }
-    console.log(roles)
+    console.table(employees)
   })
+}
+inquirer.prompt(prompts).then(handleMainPrompts)
 
-db.query(`
-  SELECT employees.id, employees.firstName, employees.lastName, roles.title, roles.salary, departments.name AS department, CONCAT(manager.firstName, ' ', manager.lastName) AS manager
-  FROM employees LEFT JOIN roles ON employees.roleId = roles.id
-  LEFT JOIN departments ON roles.departmentId = departments.id
-  LEFT JOIN employees manager on manager.id = employees.managerId
-`, (err, employees) => {
-  if (err) { console.log(err) }
-  console.log(employees)
-})
+
+
+// const db = mysql.createConnection('mysql://root:rootroot@localhost/employees_db')
+
+
+
+// db.query(`
+//   SELECT roles.id, roles.title, roles.salary, departments.name AS department
+//   FROM roles
+//   LEFT JOIN departments
+//   ON roles.departmentId = departments.id
+//   `, (err, roles) => {
+//     if (err) { console.log(err) }
+//     console.log(roles)
+//   })
+
+// db.query(`
+//   SELECT employees.id, employees.firstName, employees.lastName, roles.title, roles.salary, departments.name AS department, CONCAT(manager.firstName, ' ', manager.lastName) AS manager
+//   FROM employees LEFT JOIN roles ON employees.roleId = roles.id
+//   LEFT JOIN departments ON roles.departmentId = departments.id
+//   LEFT JOIN employees manager on manager.id = employees.managerId
+// `, (err, employees) => {
+//   if (err) { console.log(err) }
+//   console.log(employees)
+// })
