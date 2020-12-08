f5c8cd18c5622cbe3618d2f523a3b6cbbfe50d02
Joseph Wong-Ludwiczak <josclud@gmail.com>
2020-12-07 23:44:10 -0800

    added files

--- employees.js
+++ employees.js
@@ -7,27 +7,99 @@ const db = mysql.createConnection('mysql://root:Monkey23@localhost/employees_db'
 let prompts = [
   {
     type: 'list',
-    name: 'name',
+    name: 'mainPrompt',
     message: 'What would you like to view?',
     choices: [
       {
         name: 'View all employees',
         value: 'view employees'
       },
-      // {
-      //   name: 'View all Departments',
-      //   value: 'view departments'
-      // }
+      {
+        name: 'View all Departments',
+        value: 'view departments'
+      }, 
+      {
+        name: 'View Roles',
+        value: 'view roles'
+      },
+      {
+      name: 'Add New Employee',
+      value: 'add employee'
+    }
     ]
 
   }
 ]
-function handleMainPrompts(answers) {
-  console.log(answers.name);
-  db.query('SELECT * FROM employees', (err, employees) => {
+
+const employeePrompts = [
+  {
+    type: 'input',
+    name: 'firstName',
+    message: 'What is the employees first name?'
+  },
+  {
+    type: 'input',
+    name: 'lastName',
+    message: 'What is the employees last name?'
+  },
+  {
+    type: 'list',
+    name: 'addDepartment',
+    message: 'What department does this employee work in?',
+    choices: ['Engineering', 'Finance', 'Legal', 'Sales']
+  },
+  {
+    type: 'list',
+    name: 'addRole',
+    message: 'What is this employees role?',
+    choices: ['Sales Lead', ' Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', ' Lawyer']
+  },
+  
+
+]
+
+function employeeInfo(table) {
+    console.log('hi');
+    db.query(`SELECT * FROM ${table}`, (err, res) => {
     if (err) { console.log(err) }
-    console.table(employees)
+    console.table(res)
+    return res
   })
+}
+
+function getEmployeeData() {
+  return inquirer.prompt(employeePrompts)
+  
+}
+
+function addEmployee(data) {
+  console.log(data);
+  db.query(`INSERT INTO ${table}(data.firstname, data.lastName, data.rol)`)
+  firstName, lastName, roleId, managerId
+}
+
+function handleMainPrompts(answers) {
+  console.log(answers.mainPrompt);
+  switch (answers.mainPrompt) {
+    case 'view employees':
+      console.log('hello');
+      employeeInfo('employees');
+      break;
+    case 'view departments':
+      console.log('view departments');
+      employeeInfo('departments');
+      break;
+    case 'view roles':
+      employeeInfo('roles')
+      break;
+    case 'add employee':
+      getEmployeeData().then((res) => {
+        addEmployee(res)
+      })
+      
+    default:
+      break;
+  }
 }
 inquirer.prompt(prompts).then(handleMainPrompts)
 
